1e730267711307803d5c09388189308e00234f6d
Davi Mica <davimica04@gmail.com>
2024-03-22 14:11:57 -0300

    remake do sistema de dados

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-let currentDice                                                            //variavel pra indicar qual botão foi clicado
-const pastRolls = document.getElementById("past_rolls") 
-const dice = document.getElementsByClassName("die")                        //dice = lista de todos os botões
+const signs = document.getElementsByClassName("sign");
+let signValue;
+let dataSides;
+let diceAmountDisplay;
 
-for(let x = 0; x < dice.length; x++){ 
-    dice[x].addEventListener("click", function(e){                         //loop cria um Event Listener em todos os botões
-        currentDice = dice[x].getAttribute("data-sides")                   //passa o valor do data-sides para a variavel currentDice
-        RollDice()                                                        
+for(let i = 0; i < signs.length; i++){
+    signs[i].addEventListener('click', function (e){
+        signValue = signs[i].innerHTML
+        dataSides = signs[i].getAttribute("data-sides")
+        diceAmountDisplay = document.querySelector("p[data-sides='" + dataSides + "']")
+        ChangeDiceAmount()
     })
 }
 
-function RollDice(){
-    const roll = document.getElementById("result").value = Math.ceil(Math.random() * currentDice); //escolhe um valor entre 1 e currentDice e armazena em roll
-    RegisterRoll(roll)
-}
+function ChangeDiceAmount(){
+    diceAmountDisplay.innerHTML = parseInt(diceAmountDisplay.innerHTML) + parseInt(signValue+1)
+    if(diceAmountDisplay.innerHTML < 0){
+        diceAmountDisplay.innerHTML = 0;
+    }
 
-function RegisterRoll(value){
-    const newRoll = document.createElement("p")     //cria um elemento
-    newRoll.innerHTML = value                       //atribui a esse elemento o valor que caiu no dado
-    pastRolls.prepend(newRoll)                      //inseri o elemento como primeiro filho da div past_rolls
 }
-
